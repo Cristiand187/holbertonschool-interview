abea275d8548cf6711aa297bddc07d210783d70d
cristiand187 <cristiand187@hotmail.com>
2021-12-09 21:36:29 -0500

    Task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,4 +4,12 @@
 
 def canUnlockAll(boxes):
     """"""
-    print(boxes)
+    listKeys = [0]
+    for mykey in listKeys:
+        for boxKey in boxes[mykey]:
+            if boxKey not in listKeys and boxKey < len(boxes):
+                listKeys.append(boxKey)
+
+    if len(listKeys) == len(boxes):
+        return True
+    return False
